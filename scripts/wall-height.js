b67815130daefd42dc21af53d3636c9e09e56671
theripper93 <tsplab@gmail.com>
2022-04-02 15:24:00 +0200

    a

--- scripts/wall-height.js
+++ scripts/wall-height.js
@@ -174,5 +174,52 @@ Handlebars.registerHelper('if_null', function(a, opts) {
     }
 });
 
+// First time message
 
+Hooks.once("ready", () => {
+    // Module title
+    const MODULE_TITLE = game.modules.get(MODULE_ID).data.title;
+  
+    const FALLBACK_MESSAGE_TITLE = "IMPORTANT - Wall Height 4";
+    const FALLBACK_MESSAGE = `<large>
+    <p><strong>I (theripper93) am now taking over the developement of Wall Height, be sure to stop by my <a href="https://theripper93.com/">Discord</a> for help and support from the wonderful community as well as many resources</strong></p>
+  
+    <h1>Wall Height Migration</h1>
+    <p>Due to some incorrect data structures the Wall Height data needs to be migrated, this process will happen automatically for all your scenes and compendiums, if you need to run the migration again you can do so in the module settings. <strong>This new version of Wall Height requires Levels, Better Roofs, Token Attacher and 3D Canvas to be updated as well!</strong></p>
+
+    <p>Thanks to all the patreons supporting the development of my modules making continued updates possible!</p>
+    <p>If you want to support the development of the module or get customized support in setting up your maps you can do so here : <a href="https://www.patreon.com/theripper93">Patreon</a> </p></large>
+    <p><strong>Patreons</strong> get also access to <strong>15+ premium modules</strong></p>
+    <p>Want even more verticality? Go Full 3D</p>
+    <h1>3D Canvas</h1>
+    <iframe width="385" height="225" src="https://www.youtube.com/embed/hC1QGZFUhcU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <p>Check 3D Canvas and all my other <strong>15+ premium modules <a href="https://theripper93.com/">Here</a></strong></p>`;
+  
+    // Settings key used for the "Don't remind me again" setting
+    const DONT_REMIND_AGAIN_KEY = "popup-dont-remind-again";
+  
+    // Dialog code
+    game.settings.register(MODULE_ID, DONT_REMIND_AGAIN_KEY, {
+      name: "",
+      default: false,
+      type: Boolean,
+      scope: "world",
+      config: false,
+    });
+    if (game.user.isGM && !game.settings.get(MODULE_ID, DONT_REMIND_AGAIN_KEY)) {
+      new Dialog({
+        title: FALLBACK_MESSAGE_TITLE,
+        content: FALLBACK_MESSAGE,
+        buttons: {
+          ok: { icon: '<i class="fas fa-check"></i>', label: "Understood" },
+          dont_remind: {
+            icon: '<i class="fas fa-times"></i>',
+            label: "Don't remind me again",
+            callback: () =>
+              game.settings.set(MODULE_ID, DONT_REMIND_AGAIN_KEY, true),
+          },
+        },
+      }).render(true);
+    }
+  });
 
